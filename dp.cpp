4d1eb944f370d28e742193180f16132a07281057
akash844 <akash.17bit1125@abes.ac.in>
2019-03-21 11:58:33 +0530

    dp

---
+++ dp.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+long long int fibo(int n,long long int fib[])
+{
+	if (n==1 || n==2)
+		return 1;
+	else if (fib[n]!=0)
+		return fib[n];
+	else
+	{
+		fib[n]=fibo(n-1,fib)+fibo(n-2,fib);
+		return fib[n];
+	}
+		
+}
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		long long int fib[n+2]={0};
+		long long int ans=fibo(n,fib);
+		cout<<ans<<endl;
+	}
+}
